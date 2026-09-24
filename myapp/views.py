4b6c69e32b4b44bb32f836f17b17765e206f2f66
Alex <a0982135416@gmail.com>
2026-09-24 11:06:12 +0800

    B-branch bweb finish

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 
 def home(request):
     return render(request, 'index.html')
+
+
+def about(request):
+    return HttpResponse("This is the about page.")
