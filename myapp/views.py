a865ab4a2aa26558095f6917f30940a6446d7586
Alex <a0982135416@gmail.com>
2026-09-24 11:28:43 +0800

    A-branch aweb finish

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def home(request):
     return render(request, 'index.html')
 def aweb(request):
-    return render(request, 'aweb.html')
+    return render(request, 'aweb.html完成')
