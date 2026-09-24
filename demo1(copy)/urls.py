b694cb2e7f094a684d0447b8aee98d60b8693e7a
Alex <a0982135416@gmail.com>
2026-09-24 11:43:33 +0800

    A-branch aweb finish2.0

--- demo1(copy)/urls.py
+++ demo1(copy)/urls.py
@@ -21,6 +21,6 @@ from myapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
+    # path('', views.home, name='home'),
     path('aweb/', views.aweb, name='aweb'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def home(request):
-    return render(request, 'index.html')
+# def home(request):
+#     return render(request, 'index.html')
 def aweb(request):
-    return render(request, 'aweb.html完成')
+    return HttpResponse("aweb.html完成")
