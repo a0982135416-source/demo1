d84ac064283971d9f64cb35792713bd803ef9617
Alex <a0982135416@gmail.com>
2026-09-24 13:31:11 +0800

    B-branch

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def home(request):
     return render(request, 'index.html')
+def cweb(request):
+    return HttpResponse("This is the cweb page.完成")
